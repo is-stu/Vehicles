1ea286a0d78b394833f2f7d4520453e9bced2bb4
Stewar Marin <stewar1020@gmail.com>
2021-08-30 19:31:54 -0500

    Created project & added first page vehicle types

---
+++ Vehicles/Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        public int id { get; set; }
+
+        [Display(Name = "Vehicle type")]
+        [MaxLength(50, ErrorMessage = "The field {0} just allow {1} characters")]
+        [Required(ErrorMessage = "The field {0} is required")] 
+        public string description { get; set; }
+    }
+}
---
+++ Vehicles/Vehicles.API/Migrations/20210831001837_AddTableVehicleType.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicles.API.Migrations
+{
+    public partial class AddTableVehicleType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VehiclesTypes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    description = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehiclesTypes", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehiclesTypes_description",
+                table: "VehiclesTypes",
+                column: "description",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VehiclesTypes");
+        }
+    }
+}
